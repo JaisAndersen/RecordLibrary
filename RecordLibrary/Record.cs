696fb64473af0400647d629a9eac73ef6378d571
Jais Andersen <jais.h.sorensen@gmail.com>
2025-04-28 10:00:37 +0200

    Model klasse + Validation

---
+++ RecordLibrary/Record.cs
@@ -0,0 +1,46 @@
+﻿namespace RecordLibrary
+{
+    public class Record
+    {
+        private string title;
+        private string artist;
+        private int duration;
+
+        public int Id { get; set; }
+        public string Title
+        {
+            get => title;
+            set
+            {
+                if (value == null)
+                    throw new ArgumentNullException("Title is null");
+                if (value.Length < 1)
+                    throw new ArgumentOutOfRangeException("Title must be at least 1 charachter long");
+            }
+        }
+        public string Artist
+        {
+            get => artist;
+            set
+            {
+                if (value == null)
+                    throw new ArgumentNullException("Artist is null");
+                if (value.Length < 1)
+                    throw new ArgumentOutOfRangeException("Artist must be at least 1 charachter long");
+                artist = value;
+            }
+        }
+        public int Duration
+        {
+            get => duration;
+            set
+            {
+                if (value < 0)
+                    throw new ArgumentOutOfRangeException("Duration must be a positive number");
+                if (value > 7200)
+                    duration = value;
+            }
+        }
+        public int PublicationYear { get; set; }
+    }
+}
