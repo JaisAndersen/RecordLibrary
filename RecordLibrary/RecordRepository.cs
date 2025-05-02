526456e6e3a758727aab9f27e34d437f653fd360
Jais Andersen <jais.h.sorensen@gmail.com>
2025-05-02 12:43:42 +0200

    RecordRepo added

---
+++ RecordLibrary/RecordRepository.cs
@@ -0,0 +1,16 @@
+﻿using RecordLibrary.Models;
+
+namespace RecordLibrary
+{
+    public class RecordRepository
+    {
+        private List<Record> records = new();
+
+        public List<Record> GetRecords()
+        {
+            List<Record> result = new List<Record>(records);
+            return result;
+        }
+
+    }
+}
